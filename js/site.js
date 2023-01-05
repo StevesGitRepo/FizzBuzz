3ac14c821e68a72967b4a35ce26d6941330b4c5c
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-01-04 23:19:15 -0700

    App, site.js pages started

--- js/site.js
+++ js/site.js
@@ -1,20 +1,69 @@
-// Call Hello World
-function helloWorld() {
-  alert('Hello World');
-}
+//get values from the page for start and ending inputs
+const getValues = () => {
+  let startValue = document.getElementById('startValue').value;
+  let endValue = document.getElementById('endValue').value;
+  let numbers = generateNumbers(startValue, endValue);
 
-let first = 'Steve';
-let last = 'Szymanski';
-let msg = `Hello + ${first} ${last}`;
+  //input validation, parse into integers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
 
-console.log(msg);
+  //integer check:
+  //alert('The sum is:' (startValue + endValue));
 
-let city = 'Phoenix';
-let state = 'AZ';
-let location = `You are in ${city}, ${state}`;
-console.log(location);
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    //generateNumbers
+  } else {
+    alert('You must enter integers');
+  }
 
-let name = 'Steve Szymanski';
+  //range limits check and alert message
+  if (startValue < 1 || endValue > 100) {
+    alert('Type a number between 1 and 100');
+  }
 
-name.substring(6,9) // "Szym"
-name.substring(8,11) //"yman"
+  displayNumbers(numbers);
+};
+
+const generateNumbers = (sValue, eValue) => {
+  let numbers = [];
+
+  for (let index = sValue; index < eValue; index++) {
+    numbers.push[index];
+  }
+
+  return numbers;
+};
+
+const displayNumbers = (numbers) => {
+  let templateRows = '';
+
+  for (let i = 0; i < numbers.length; i++) {
+    let className = number;
+    let number = numbers[i];
+
+    if (number % 1 === 0) {
+      let number = numbers[i];
+    } else {
+      if (number % 3 === 0) {
+        className = 'Fizz';
+      }
+
+      if (number % 5 === 0) {
+        className = 'Buzz';
+      }
+
+      if (number % 15 === 0) {
+        className = 'FizzBuzz';
+      }
+    }
+
+    templateRows += `<tr><td class="${className}">${number}</td>
+    <td class="${className}">${number}</td>
+    <td class="${className}">${number}</td>
+    <td class="${className}">${number}</td>
+    <td class="${className}">${number}</td></tr>`;
+  }
+
+  document.getElementById('results').innerHTML = templateRows;
+};
